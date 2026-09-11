69902dfe3630ca0f397d69031999ab24ca6be387
SteveMan67 <timslawncare.ok@gmail.com>
2026-09-11 14:01:39 -0500

    fix: correct getter/setter definitions in component.ts

--- ui/ts/components/component.ts
+++ ui/ts/components/component.ts
@@ -1,17 +1,19 @@
 export abstract class Component<T extends object> extends HTMLElement {
   props: T;
+  private values: T;
 
   constructor(defaultProps: T) {
     super();
 
     this.props = defaultProps;
+    this.values = { ...defaultProps };
 
     for (const key of Object.keys(defaultProps) as (keyof T)[]) {
-      Object.defineProperty(this, key, {
-        get: () => this.props[key],
+      Object.defineProperty(this.props, key, {
+        get: () => this.values[key],
 
         set: (value: T[typeof key]) => {
-          this.props[key] = value;
+          this.values[key] = value;
           this.update();
         },
 
